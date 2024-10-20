ebd21ab0d8f209f1c7b3a0a7de882b132017ecbb
steadman1 <steadman@vt.edu>
2024-10-19 20:40:27 -0400

    modified the serifBold function

--- Sources/SpenceKit/Extensions.swift
+++ Sources/SpenceKit/Extensions.swift
@@ -76,14 +76,60 @@ extension Font {
 }
 
 @available(iOS 13.0, *)
-public extension Text {
+public extension View {
+    @available(iOS 15.0, *)
     @MainActor func serifBold(font: Font.SpenceKit.FontSkeleton, strokeColor: Color = .black) -> some View {
         self
             .font(.custom(font.name, size: font.size))
             .overlay(
                 self
                     .font(.custom(font.name, size: font.size))
-                    .shadow(color: strokeColor, radius: 2) // Adjust stroke width
+                    .stroke(color: strokeColor, width: font.size / 10) // Adjust stroke width
             )
+    }
+    
+    @available(iOS 15.0, *)
+    func stroke(color: Color, width: CGFloat = 1) -> some View {
+        modifier(StrokeModifier(strokeSize: width, strokeColor: color))
+    }
+}
+
+@available(iOS 15.0, *)
+public struct StrokeModifier: ViewModifier {
+    private let id = UUID()
+    var strokeSize: CGFloat = 1
+    var strokeColor: Color = .blue
+
+    public func body(content: Content) -> some View {
+        if strokeSize > 0 {
+            appliedStrokeBackground(content: content)
+        } else {
+            content
+        }
+    }
+
+    private func appliedStrokeBackground(content: Content) -> some View {
+        content
+            .padding(strokeSize*2)
+            .background(
+                Rectangle()
+                    .foregroundColor(strokeColor)
+                    .mask(alignment: .center) {
+                        mask(content: content)
+                    }
+            )
+    }
+
+    func mask(content: Content) -> some View {
+        Canvas { context, size in
+            context.addFilter(.alphaThreshold(min: 0.01))
+            if let resolvedView = context.resolveSymbol(id: id) {
+                context.draw(resolvedView, at: .init(x: size.width/2, y: size.height/2))
+            }
+        } symbols: {
+            content
+                .tag(id)
+                .blur(radius: strokeSize)
+        }
     }
 }

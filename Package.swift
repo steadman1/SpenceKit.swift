13fa2c02e0602c2aea5b48eb62a71c9fb8f2d8e9
steadman1 <steadman@vt.edu>
2024-11-02 15:45:25 -0400

    finished InlineTextField

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SpenceKit",
-    platforms: [.iOS("15.0"), .macOS("10.15")],
+    platforms: [.iOS("15.0"), .macOS("11.0")],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- Sources/SpenceKit/Elements/Free Input/InlineTextInput.swift
+++ Sources/SpenceKit/Elements/Free Input/InlineTextInput.swift
@@ -7,12 +7,122 @@
 
 import SwiftUI
 
-struct InlineTextInput: View {
-    var body: some View {
-        TextField("Enter text here", text: .constant(""))
+public struct InlineTextField<IdentifierContent: View, HelperContent: View>: View {
+    
+    @Binding public var text: String
+    
+    private let title: String
+    private let description: String
+    private let identifier: IdentifierContent
+    private let hasIdentifier: Bool
+    private let helperButton: HelperContent
+    
+    public init(
+        _ text: Binding<String>
+    ) where IdentifierContent == EmptyView, HelperContent == EmptyView {
+        self._text = text
+        self.title = ""
+        self.description = ""
+        self.identifier = EmptyView()
+        self.hasIdentifier = false
+        self.helperButton = EmptyView()
+    }
+    
+    public init(
+        _ text: Binding<String>,
+        title: String,
+        description: String
+    ) where IdentifierContent == EmptyView, HelperContent == EmptyView {
+        self._text = text
+        self.title = title
+        self.description = description
+        self.identifier = EmptyView()
+        self.hasIdentifier = false
+        self.helperButton = EmptyView()
+    }
+    
+    public init(
+        _ text: Binding<String>,
+        title: String,
+        description: String,
+        @ViewBuilder identifier: @escaping () -> IdentifierContent,
+        @ViewBuilder helperButton: @escaping () -> HelperContent
+    ) {
+        self._text = text
+        self.title = title
+        self.description = description
+        self.identifier = identifier()
+        self.hasIdentifier = true
+        self.helperButton = helperButton()
+    }
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                if !title.isEmpty {
+                    HStack {
+                        Text(title)
+                            .font(.SpenceKit.SansHeadlineFont)
+                            .foregroundStyle(Color.SpenceKit.SecondaryText)
+                            .padding(.bottom, SpenceKit.Constants.padding8)
+                        Spacer()
+                    }
+                }
+                HStack(spacing: 0) {
+                    identifier
+                        .foregroundStyle(Color.SpenceKit.TertiaryText)
+                        .padding(.leading, SpenceKit.Constants.padding16)
+                    
+                    ZStack(alignment: .leading) {
+                        if text.isEmpty {
+                            Text("Search...")
+                                .font(.SpenceKit.SansHeadFont)
+                                .foregroundColor(Color.SpenceKit.TertiaryText)
+                        }
+                        TextField("", text: $text)
+                            .font(.SpenceKit.SansHeadFont)
+                    }.padding(.horizontal,
+                              SpenceKit.Constants.padding16 - (hasIdentifier ? SpenceKit.Constants.padding8 : 0) )
+                    
+                    Spacer()
+                    
+                    helperButton
+                        .padding(.trailing, SpenceKit.Constants.padding16)
+                    
+                }.frame(maxWidth: .infinity)
+                    .frame(height: 48)
+                    .background(Color.SpenceKit.Background)
+                    .clipShape(RoundedRectangle(cornerRadius: SpenceKit.Constants.cornerRadiusMAX))
+                    .stroke(color: Color.SpenceKit.Border,
+                         width: SpenceKit.Constants.borderWidth)
+                    .clipShape(RoundedRectangle(cornerRadius: SpenceKit.Constants.cornerRadiusMAX + SpenceKit.Constants.borderWidth * 2))
+                
+                HStack {
+                    if !description.isEmpty {
+                        HStack {
+                            Text(description)
+                                .font(.SpenceKit.SansSubheadFont)
+                                .foregroundStyle(Color.SpenceKit.TertiaryText)
+                                .padding(.top, SpenceKit.Constants.padding8)
+                            Spacer()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
+@available(iOS 17.0, *)
 #Preview {
-    
+    @Previewable @State var text: String = ""
+    InlineTextField($text)
+    InlineTextField($text, title: "title!", description: "more info...")
+    InlineTextField($text, title: "title!", description: "more info...") {
+        SFIcon("arrow.up.right", size: .head)
+    } helperButton: {
+        HelperButtons.Info {
+            print()
+        }
+    }
 }

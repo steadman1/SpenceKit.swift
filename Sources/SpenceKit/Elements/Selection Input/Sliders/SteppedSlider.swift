33d967e702ac5564ec2368a7e732f94b74b43b7a
steadman1 <steadman@vt.edu>
2024-11-02 14:57:08 -0400

    removed unfinished SteppedSlider

--- Sources/SpenceKit/Elements/Selection Input/Sliders/SteppedSlider.swift
+++ Sources/SpenceKit/Elements/Selection Input/Sliders/SteppedSlider.swift
@@ -7,38 +7,40 @@
 
 import SwiftUI
 
-@available(iOS 17.0, *)
-public struct SteppedSlider: View {
-    public enum Count: Int {
-        case single = 0
-        case double = 1
-    }
-    // Slider value binding
-    @Binding private var selection: Double
-    
-    private let thumbCount: SteppedSlider.Count
-    
-    public init(_ selection: Binding<Double>) {
-        self._selection = selection
-    }
-    
-    private let range: ClosedRange<Double> = 0...1
-    private let thumbHeight: CGFloat = 20
-    private let trackHeight: CGFloat = 4
-    
-    public var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                
-            }
-        }
-    }
-}
+/*
+ @available(iOS 17.0, *)
+ public struct SteppedSlider: View {
+     public enum Count: Int {
+         case single = 0
+         case double = 1
+     }
+     // Slider value binding
+     @Binding private var selection: Double
+     
+     private let thumbCount: SteppedSlider.Count
+     
+     public init(_ selection: Binding<Double>) {
+         self._selection = selection
+     }
+     
+     private let range: ClosedRange<Double> = 0...1
+     private let thumbHeight: CGFloat = 20
+     private let trackHeight: CGFloat = 4
+     
+     public var body: some View {
+         GeometryReader { geometry in
+             ZStack {
+                 
+             }
+         }
+     }
+ }
 
-@available(iOS 17.0, *)
-#Preview {
-    @Previewable @State var from: Double = 0
-    @Previewable @State var to: Double = 0.4
-    SteppedSlider($from)
-    SteppedSlider(from: $from, to: $to)
-}
+ @available(iOS 17.0, *)
+ #Preview {
+     @Previewable @State var from: Double = 0
+     @Previewable @State var to: Double = 0.4
+     SteppedSlider($from)
+     SteppedSlider(from: $from, to: $to)
+ }
+ */

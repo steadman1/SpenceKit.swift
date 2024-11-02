b7dbd07babec21cac44a800edec2ab1fa4ccb577
steadman1 <steadman@vt.edu>
2024-11-02 15:40:27 -0400

    finished InlineTextField

--- Sources/SpenceKit/Elements/Free Input/InlineTextInput.swift
+++ Sources/SpenceKit/Elements/Free Input/InlineTextInput.swift
@@ -5,3 +5,14 @@
 //  Created by Spencer Steadman on 10/19/24.
 //
 
+import SwiftUI
+
+struct InlineTextInput: View {
+    var body: some View {
+        TextField("Enter text here", text: .constant(""))
+    }
+}
+
+#Preview {
+    
+}

8bee0e34e507c926646b56edf645145f54e46806
steadman1 <steadman@vt.edu>
2025-01-11 17:23:05 -0500

    added action to Tab and created TabGroup

---
+++ Sources/SpenceKit/Mixtures/Tab Groups/TabGroup.swift
@@ -0,0 +1,112 @@
+//
+//  TabGroup.swift
+//  SpenceKit
+//
+//  Created by Spencer Steadman on 1/11/25.
+//
+
+import SwiftUI
+
+public struct TabGroup: View {
+    private let title: String?
+    private let tabs: [Tab]
+    
+    public init(title: String? = nil, tabs: [Tab]) {
+        self.title = title
+        self.tabs = tabs
+    }
+    
+    public static let TAB_HEIGHT: CGFloat = 54
+    
+    public var body: some View {
+        CornerRadiusReader { radius in
+            let cornerRadius = radius > 0 ? radius : SpenceKit.Constants.cornerRadius24
+            VStack(alignment: .leading) {
+                if title != nil {
+                    Text(title!)
+                        .font(Font.SpenceKit.SansHintFont)
+                        .foregroundStyle(Color.SpenceKit.TertiaryText)
+                }
+                VStack(alignment: .leading, spacing: SpenceKit.Constants.spacing4) {
+                    ForEach(tabs.enumerate(), id: \.0) { index, tab in
+                        Button {
+                            tab.action()
+                        } label: {
+                            HStack {
+                                let padding = SpenceKit.Constants.padding16
+                                ZStack {
+                                    tab.activeIcon
+                                        .font(Font.SpenceKit.SansHeadlineFont)
+                                        .foregroundStyle(Color.SpenceKit.PrimaryAccent)
+                                        .padding(padding)
+                                }.frame(
+                                    width: TabGroup.TAB_HEIGHT - padding,
+                                    height: TabGroup.TAB_HEIGHT - padding
+                                ).background(Color.SpenceKit.SecondaryAccent)
+                                    .clipShape(RoundedRectangle(cornerRadius: SpenceKit.Constants.cornerRadius24 - padding))
+                                    
+                                
+                                Text(tab.title)
+                                    .font(Font.SpenceKit.SansBodyFont)
+                                    .foregroundStyle(Color.SpenceKit.PrimaryText)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                                    .font(Font.SpenceKit.SansHeadlineFont)
+                                    .foregroundStyle(Color.SpenceKit.TertiaryText)
+                            }.frame(height: TabGroup.TAB_HEIGHT)
+                        }
+                        
+                        if index < tabs.count - 1 {
+                            HDivider()
+                        }
+                    }
+                }.frame(maxWidth: .infinity,
+                           maxHeight: CGFloat(tabs.count) * TabGroup.TAB_HEIGHT + CGFloat(tabs.count) * SpenceKit.Constants.borderWidth + CGFloat(tabs.count * 2 - 1) * SpenceKit.Constants.spacing4)
+                    .padding(.vertical, SpenceKit.Constants.padding4)
+                    .padding(.horizontal, SpenceKit.Constants.padding16)
+                    .background(Color.SpenceKit.Background)
+                    .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+                    .stroke(color: Color.SpenceKit.Border,
+                            width: SpenceKit.Constants.borderWidth)
+                    .clipShape(RoundedRectangle(cornerRadius: cornerRadius + SpenceKit.Constants.borderWidth))
+            }
+        }
+    }
+}
+
+#Preview {
+    TabGroup(
+        title: "General",
+        tabs: [
+            Tab(
+                title: "Tab 1",
+                icon: .init(systemName: "paintbrush")
+               ),
+            Tab(
+                title: "Tab 2",
+                icon: .init(systemName: "paintbrush")
+               ),
+            Tab(
+                title: "Tab 3",
+                icon: .init(systemName: "paintbrush")
+               ),
+        ]
+    ).padding(.horizontal, 16)
+    
+    TabGroup(
+        title: "Specific",
+        tabs: [
+            Tab(
+                title: "Tab 1",
+                icon: .init(systemName: "paintbrush")
+               ),
+            Tab(
+                title: "Tab 2",
+                icon: .init(systemName: "paintbrush")
+               ),
+            
+        ]
+    ).padding(.horizontal, 16)
+}
--- Sources/SpenceKit/Structures/Tab.swift
+++ Sources/SpenceKit/Structures/Tab.swift
@@ -15,10 +15,23 @@
 
 import SwiftUI
 
-public struct Tab {
+public struct Tab: Identifiable {
+    public let id: UUID = UUID()
     public let title: String
     public let activeIcon: Image
     public let inactiveIcon: Image
+    public let action: () -> Void
+    
+    public init(
+        title: String,
+        icon: Image,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.activeIcon = icon
+        self.inactiveIcon = icon
+        self.action = action
+    }
     
     public init(
         title: String,
@@ -27,6 +40,19 @@ public struct Tab {
         self.title = title
         self.activeIcon = icon
         self.inactiveIcon = icon
+        self.action = { }
+    }
+    
+    public init(
+        title: String,
+        activeIcon: Image,
+        inactiveIcon: Image,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.activeIcon = activeIcon
+        self.inactiveIcon = inactiveIcon
+        self.action = action
     }
     
     public init(
@@ -37,5 +63,6 @@ public struct Tab {
         self.title = title
         self.activeIcon = activeIcon
         self.inactiveIcon = inactiveIcon
+        self.action = { }
     }
 }

285f9213509ca58d03fa63235e40de3975afa618
spence <64872573+steadman1@users.noreply.github.com>
2025-06-14 16:23:04 -0400

    Update CornerRadiusModifier.swift

--- Sources/SpenceKit/Structures/CornerRadiusModifier.swift
+++ Sources/SpenceKit/Structures/CornerRadiusModifier.swift
@@ -5,9 +5,9 @@
 //  Created by Spencer Steadman on 1/16/25.
 //
 
-#if canImport(SwiftUICore)
+#if canImport(SwiftUI)
 
-import SwiftUICore
+import SwiftUI
 
 /// Defines CornerRadiusModifier to inject the corner radius into the environment
 public struct CornerRadiusModifier: ViewModifier {

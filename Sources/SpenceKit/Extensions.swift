43c9518598afb47e6e1b5eb7266f74224d5c8c00
spence <64872573+steadman1@users.noreply.github.com>
2026-01-13 17:40:51 -0500

    Refactor StrokeModifier for simplified stroke rendering
    Refactor StrokeModifier to simplify stroke application and remove unused mask function.

--- Sources/SpenceKit/Extensions.swift
+++ Sources/SpenceKit/Extensions.swift
@@ -500,44 +500,25 @@ public extension View {
     }
 }
 
-// from "paescebu" on StackOverflow
-// https://stackoverflow.com/questions/57334125/how-to-make-text-stroke-in-swiftui
 public struct StrokeModifier: ViewModifier {
-    private let id = UUID()
     var strokeSize: CGFloat = 1
     var strokeColor: Color = .blue
 
     public func body(content: Content) -> some View {
-        if strokeSize > 0 {
-            appliedStrokeBackground(content: content)
-        } else {
-            content
-        }
-    }
-
-    private func appliedStrokeBackground(content: Content) -> some View {
         content
-            .padding(strokeSize * 2)
             .background(
-                Rectangle()
-                    .foregroundColor(strokeColor)
-                    .mask(alignment: .center) {
-                        mask(content: content)
+                ZStack {
+                    ForEach(0..<8) { index in
+                        let angle = Double(index) * (Double.pi / 4) // 45 degrees * index
+                        let x = strokeSize * cos(angle)
+                        let y = strokeSize * sin(angle)
+                        
+                        content
+                            .foregroundColor(strokeColor)
+                            .offset(x: x, y: y)
                     }
+                }
             )
-    }
-
-    private func mask(content: Content) -> some View {
-        Canvas { context, size in
-            context.addFilter(.alphaThreshold(min: 0.01))
-            if let resolvedView = context.resolveSymbol(id: id) {
-                context.draw(resolvedView, at: .init(x: size.width/2, y: size.height/2))
-            }
-        } symbols: {
-            content
-                .tag(id)
-                .blur(radius: strokeSize)
-        }
     }
 }
 

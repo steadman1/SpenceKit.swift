b47499dca68186be6366b4fca94f8989684009eb
spence <64872573+steadman1@users.noreply.github.com>
2026-01-13 18:09:36 -0500

    Refactor serifBold to use textStroke method
    Updated the serifBold function to use textStroke instead of stroke. Added a new textStroke function for text outlining.

--- Sources/SpenceKit/Extensions.swift
+++ Sources/SpenceKit/Extensions.swift
@@ -462,11 +462,15 @@ public extension View {
     func serifBold(font: Font.SpenceKit.FontSkeleton, strokeColor: Color = .black) -> some View {
         self
             .font(.custom(font.name, size: font.size))
-            .stroke(color: strokeColor, width: font.size / 80)
+            .textStroke(color: strokeColor, width: font.size / 80)
     }
     
     func stroke(color: Color, width: CGFloat = SpenceKit.Constants.borderWidth) -> some View {
         modifier(StrokeModifier(strokeSize: width / 2, strokeColor: color))
+    }
+
+    func textStroke(color: Color, width: CGFloat = SpenceKit.Constants.borderWidth) -> some View {
+        modifier(TextStrokeModifier(strokeSize: width / 2, strokeColor: color))
     }
     
     func border() -> some View {
@@ -500,7 +504,48 @@ public extension View {
     }
 }
 
+// from "paescebu" on StackOverflow
+// https://stackoverflow.com/questions/57334125/how-to-make-text-stroke-in-swiftui
 public struct StrokeModifier: ViewModifier {
+    private let id = UUID()
+    var strokeSize: CGFloat = 1
+    var strokeColor: Color = .blue
+
+    public func body(content: Content) -> some View {
+        if strokeSize > 0 {
+            appliedStrokeBackground(content: content)
+        } else {
+            content
+        }
+    }
+
+    private func appliedStrokeBackground(content: Content) -> some View {
+        content
+            .padding(strokeSize * 2)
+            .background(
+                Rectangle()
+                    .foregroundColor(strokeColor)
+                    .mask(alignment: .center) {
+                        mask(content: content)
+                    }
+            )
+    }
+
+    private func mask(content: Content) -> some View {
+        Canvas { context, size in
+            context.addFilter(.alphaThreshold(min: 0.01))
+            if let resolvedView = context.resolveSymbol(id: id) {
+                context.draw(resolvedView, at: .init(x: size.width/2, y: size.height/2))
+            }
+        } symbols: {
+            content
+                .tag(id)
+                .blur(radius: strokeSize)
+        }
+    }
+}
+
+public struct TextStrokeModifier: ViewModifier {
     var strokeSize: CGFloat = 1
     var strokeColor: Color = .blue
 
